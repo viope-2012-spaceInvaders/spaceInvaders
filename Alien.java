3b8c1fb42527b5805f44a408fddc0aa417a66803
Paolo Boldi <paolo.boldi@unimi.it>
2013-05-15 10:03:56 +0300

    Paolo was here, again! (This time from eclipse, though, using egit)

--- Alien.java
+++ Alien.java
@@ -1,5 +1,5 @@
 /*Alien is an abstract class extending BattleFieldElement, and representing all alien
-elements at play in the game. Paolo was here! */
+elements at play in the game. Paolo was here! Paolo was here again! */
 
 public abstract class Alien extends BattleFieldElement{
 
