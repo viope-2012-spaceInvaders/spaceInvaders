7e5ccabd0cb92594e1b54a7e9b5e85003ef1d202
Paolo Boldi <paolo.boldi@unimi.it>
2013-05-15 10:23:07 +0300

    Paolo is playing with eGit+eclipse

---
+++ edu/ZbIIIZb/spaceinvaders/AStupidClassThatPaoloCreatedForFun.java
@@ -0,0 +1,12 @@
+package edu.ZbIIIZb.spaceinvaders;
+
+public class AStupidClassThatPaoloCreatedForFun {
+
+	/**
+	 * @param args
+	 */
+	public static void main( String[] args ) {
+		System.out.println( "The sun keeps shining over Leppavaara" ); 
+	}
+
+}
