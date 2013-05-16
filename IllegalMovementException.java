97e425b5164a4b72b7cf557ce63e180ad5527cc8
Kirsty Louise Deary <kdeary200@caledonian.ac.uk>
2013-05-16 15:28:05 +0300

    java doc for illegal movement exception

--- IllegalMovementException.java
+++ IllegalMovementException.java
@@ -1,4 +1,6 @@
-
+/**
+ * The IllegalMovementException prints out a message when an illegal movement is made.
+ */
 public class IllegalMovementException {
 	public IllegalMovementException(String msg) {
 		System.out.println(msg);
