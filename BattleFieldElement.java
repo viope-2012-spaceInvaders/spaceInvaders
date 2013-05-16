3b7d0c3064f4c7760d1cf5fb82042dec2a9cf968
Tamar Everson <tevers200@caledonian.ac.uk>
2013-05-16 14:41:15 +0300

    Javadoc for BattleFieldElement; Casemate and Empty

--- BattleFieldElement.java
+++ BattleFieldElement.java
@@ -1,3 +1,6 @@
+/**
+ * BattleFieldElement class
+ */
 public abstract class BattleFieldElement{
 
 	//FIELD
@@ -5,34 +8,66 @@ public abstract class BattleFieldElement{
 		protected int x;										
 	
 	//CONSTRUCTOR
+	/**
+	 * BattleFieldElement constructor.  Contains parameters for the vertical and horizontal positions.
+	 * 
+	 * @param v - vertical position of the BattleFieldElement
+	 * @param h - horizontal position of the BattleFieldElement
+	 */
 	public BattleFieldElement(int v,int h){
 		y=v;	//vertical
 		x=h;	//horizontal 
 	}
 	
 	//METHODS
+	/**
+	 * move method. moves the current position of the item to the one passed as argument. 
+	 * 
+	 * @param v - new vertical position
+	 * @param h - new horizontal position
+	 */
 	public abstract void move(int v, int h);			//moves the current position of the item
 									//to the one passed as argument. No check is made to determine if the new position
 								//is correct or outside of the battlefield margins.
 
-	
+	/**
+	 * GetY method returns the Y value
+	 * 
+	 * @return y - Returns the value instantiated
+	 */
 	 public int getY() {
 		return y;
 	}
 
+	 /**
+	  * SetY Method - Sets the selected parameter
+	  * 
+	  * @param y - This is the parameter being altered.
+	  */
 	public void setY(int y) {
 		this.y = y;
 	}
-
+	
+	
+	/**
+	 * GetX method returns the X value
+	 * 
+	 * @return X - Retrieves the inputted value of X 
+	 */
 	public int getX() {
 		return x;
 	}
 
+	/**
+	 * SetX Method - Sets the selected parameter
+	 * 
+	 * @param X - This is the parameter being altered. 
+	 */
 	public void setX(int x) {
 		this.x = x;
 	}
 
-	public abstract int getXOffset();		
+	 public abstract int getXOffset();		
 	 public abstract int getYOffset();		
 	 public abstract String toString();
 	
--- Casemate.java
+++ Casemate.java
@@ -1,20 +1,48 @@
-public class Casemate extends BattleFieldElement {
+/**
+ * This casemate class extends the BattleFieldElement
+ */
+
+	public class Casemate extends BattleFieldElement {
 
 	//FIELD
 	
 	//CONSTRUCTOR
+	
+	/**
+	 * This creates the casemate class with the parameters (v)(h)
+	 * @param v - vertical position of the casemate 
+	 * @param h - horizontal position of the casemate
+	 */
 	public Casemate(int v,int h) {
 			super(v,h);			
 	}
 	
 	//METHODS
+	
+	/**
+	 * Gets X-offset, returns the requested parameter
+	 *  
+	 * @return X - x-offset as an int
+	 */
 	public int getXOffset(){
 		return 0;
 	}
 	
+	/**
+	 * Gets Y-offset, returns the requested parameter 
+	 * 
+	 * @return Y - y-offset as an int 
+	 */
+	
 	public int getYOffset(){
 		return 0;
 	}
+	
+	/** 
+	 * This moves the casemate with the specified numbers
+	 * @param v - the vertical position of the Casemate
+	 * @param h - the horizontal position of the Casemate
+	 */
 	
 	public void move(int v, int h){
 		System.out.println("S");
