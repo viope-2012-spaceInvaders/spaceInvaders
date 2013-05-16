ee827c4456e3454fa14e7f08781b0d6750dcb194
Kirsty Louise Deary <kdeary200@caledonian.ac.uk>
2013-05-16 16:52:10 +0300

    JavaDoc
    Complete

--- IllegalElementException.java
+++ IllegalElementException.java
@@ -1,4 +1,7 @@
-
+/**
+ * This class is an extending from Exception.
+ * This class gives an notification letting the game know an illegal exception has been made.
+ */
 public class IllegalElementException extends Exception {
 	public IllegalElementException(String msg) {
 		System.out.println(msg);
