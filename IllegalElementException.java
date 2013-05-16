47dca2138ae67c497d0f6bc380e9acb565425a03
Kirsty Louise Deary <kdeary200@caledonian.ac.uk>
2013-05-16 15:11:15 +0300

    Javadoc IllegalElementException class

--- IllegalElementException.java
+++ IllegalElementException.java
@@ -1,4 +1,6 @@
-
+/**
+ * The IllegalElementException class is an extending of Exception.
+ */
 public class IllegalElementException extends Exception {
 	public IllegalElementException(String msg) {
 		System.out.println(msg);
