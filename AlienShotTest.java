fead2a615cd526a2d3102b4ed4d182ee6e60d07c
Tamar Everson <tevers200@caledonian.ac.uk>
2013-05-17 13:23:35 +0300

    Fixed errors in AlienShotTest

--- AlienShotTest.java
+++ AlienShotTest.java
@@ -1,7 +1,7 @@
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
 
 
 public class AlienShotTest {
@@ -17,16 +17,16 @@ public class AlienShotTest {
 	}
 	@Test
 	public void testMove() {
-		assertEquals("at the start x must be: 1", 1, element.getX());
-		assertEquals("at the start x must be: 3", 3, element.getY());
+		Assert.assertEquals("at the start x must be: 1", 1, element.getX());
+		Assert.assertEquals("at the start x must be: 3", 3, element.getY());
 		element.move(4,5);
-		assertEquals("After move x must be: 1", 1, element.getX());
-		assertEquals("After move x must be: 4", 4, element.getY());
+		Assert.assertEquals("After move x must be: 1", 1, element.getX());
+		Assert.assertEquals("After move x must be: 4", 4, element.getY());
 	}
 
 	@Test
 	public void testGetXOffset() {
-		assertEquals("X offset can be 0 in Empty", 0, as.getXOffset());
+		Assert.assertEquals("X offset can be 0 in Empty", 0, as.getXOffset());
 	}
 
 	@Test
@@ -40,26 +40,26 @@ public class AlienShotTest {
 			e.printStackTrace();
 		}
 		bf.setRows(2);
-		assertEquals("Verify is return is correct", 2-(2+1), as.getYOffset());
+		Assert.assertEquals("Verify is return is correct", 2-(2+1), as.getYOffset());
 	}
 
 	@Test
 	public void testToString() {
-		assertEquals("String returned must be: S", "S", as.toString());
+		Assert.assertEquals("String returned must be: S", "S", as.toString());
 	}
 
 	@Test
 	public void testAlienShot() {
-		assertEquals("The v must be: 3", 3, element.getY());
-		assertEquals("The h must be: 1", 1, element.getX());
+		Assert.assertEquals("The v must be: 3", 3, element.getY());
+		Assert.assertEquals("The h must be: 1", 1, element.getX());
 		AlienShot as = new AlienShot(2,1);
-		assertEquals("The moved must be 0 after new declaration of AlienShot", 0, as.getMoved());
+		Assert.assertEquals("The moved must be 0 after new declaration of AlienShot", 0, as.getMoved());
 	}
 
 	@Test
 	public void testGetSetMoved() {
 		as.setMoved(10);
-		assertEquals("The moved must be 10", 10, as.getMoved());
+		Assert.assertEquals("The moved must be 10", 10, as.getMoved());
 	}
 
 }
